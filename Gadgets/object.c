b7e502b1d5ab4cb087c7f70ed1794e4f099b1f09
Jay Winston <>
2018-02-25 16:53:59 +0200

    added some stuff
     Changes to be committed:
    	renamed:    Gadgets/io_mod -> Gadgets/char_mod
    	modified:   Gadgets/digiclock
    	new file:   Gadgets/object.c
    	new file:   Schooling/n2t/.gitignore
    	new file:   Schooling/n2t/HackAssembler
    	new file:   Schooling/n2t/README
    	new file:   UsefulStuff/dupfinder
    	new file:   UsefulStuff/pdfbookmaker
    	renamed:    Gadgets/report/history/patch_01-02 -> UsefulStuff/report/history/patch_01-02
    	renamed:    Gadgets/report/history/patch_02-03 -> UsefulStuff/report/history/patch_02-03
    	renamed:    Gadgets/report/history/patch_03-04 -> UsefulStuff/report/history/patch_03-04
    	renamed:    Gadgets/report/history/patch_04-05 -> UsefulStuff/report/history/patch_04-05
    	renamed:    Gadgets/report/history/patch_05-06 -> UsefulStuff/report/history/patch_05-06
    	renamed:    Gadgets/report/history/patch_06-07 -> UsefulStuff/report/history/patch_06-07
    	renamed:    Gadgets/report/history/patch_07-08 -> UsefulStuff/report/history/patch_07-08
    	renamed:    Gadgets/report/history/patch_08-09 -> UsefulStuff/report/history/patch_08-09
    	renamed:    Gadgets/report/history/patch_09-10 -> UsefulStuff/report/history/patch_09-10
    	renamed:    Gadgets/report/history/patch_10-11 -> UsefulStuff/report/history/patch_10-11
    	renamed:    Gadgets/report/history/patch_11-12 -> UsefulStuff/report/history/patch_11-12
    	renamed:    Gadgets/report/history/report-early-version -> UsefulStuff/report/history/report-early-version
    	renamed:    Gadgets/report/history/report-interim-version -> UsefulStuff/report/history/report-interim-version
    	renamed:    Gadgets/report/history/report-really-old -> UsefulStuff/report/history/report-really-old
    	renamed:    Gadgets/report/report-12 -> UsefulStuff/report/report-12
    	renamed:    Gadgets/report/report-13 -> UsefulStuff/report/report-13

---
+++ Gadgets/object.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct object {
+    int i;
+    void (*method)();  /* hack parameter check */
+};
+
+typedef struct object object;
+
+void method(object *o, int n)  /* How many parameters? */
+{
+    long p;
+    n = (int) o;
+    p = (long) &o;
+    p += (sizeof (object) + sizeof (int)) * 2;
+    o = (object *) p;
+    printf("method:    o:  %#x\n", o);
+    printf("method: o->i:  %d          <- !\n", o->i);
+    printf("method:    n:  %d\n", n);
+}
+
+object Object(int i)
+{
+    object o;
+    o.i = i;
+    o.method = method;
+    return o;
+}
+
+int main()
+{
+    object o = Object(89);
+    printf("main:    o.i:  %d\n", o.i);
+    printf("main:     &o:  %#x\n", &o);
+    o.method(4);  /* How many arguments?! */
+    return 0;
+}
