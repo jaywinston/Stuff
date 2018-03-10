c4301689035818b0c9e7e7ffedf0b9857b9d788a
Jay Winston <>
2018-03-10 23:05:20 +0200

    broke HackAssembler into modules
    Also added 'BUILTIN' to some chips.
    
     On branch master
     Changes to be committed:
    	modified:   n2t/03/a/PC.hdl
    	modified:   n2t/03/a/RAM64.hdl
    	modified:   n2t/03/a/RAM8.hdl
    	modified:   n2t/03/a/Register.hdl
    	modified:   n2t/03/b/RAM16K.hdl
    	modified:   n2t/03/b/RAM4K.hdl
    	modified:   n2t/06/HackAssembler
    	new file:   n2t/06/instruction.py
    	new file:   n2t/06/symbol.py
    	new file:   n2t/06/tables.json

---
+++ Schooling/n2t/06/instruction.py
@@ -0,0 +1,89 @@
+import json
+import re
+from symbol import install, lookup
+
+re_asm = re.compile('[01ADMEGJLNPQT=;!+&|-]{1,11}')
+
+with open('tables.json') as f:
+    tables = json.load(f)
+
+comptab = tables['comp']
+desttab = tables['dest']
+jumptab = tables['jump']
+del tables
+
+
+def int2Ahack(n):
+    return '{:016b}\n'.format(n)
+
+
+def a_cmd(asm):
+
+    expr = asm[1:]  # strip '@'
+
+    if expr.isdigit():
+        hack = int(expr)
+        
+    elif expr.startswith('R'):  # could be a RAM register
+        reg = expr[1:]  # strip 'R'
+        if reg.isdigit():
+            reg = int(reg)
+            if 0 <= reg <= 15:
+                hack = reg
+
+    else:
+        hack = lookup(expr) or install(expr)
+
+    if hack is None:
+        return None
+
+    return int2Ahack(hack)
+
+
+def c_cmd(asm):
+
+    cmd = ''.join(asm.split())
+
+    if not re_asm.fullmatch(cmd)  \
+            or cmd.count('=') > 1 \
+            or cmd.count(';') > 1:
+        return None
+
+    eq = cmd.index('=') if '=' in cmd else 0
+    sc = cmd.index(';') if ';' in cmd else len(cmd)
+
+    dest = cmd[:eq]
+    comp = cmd[eq:sc].lstrip('=')
+    jump = cmd[sc:].lstrip(';')
+
+    hack = '111'
+
+    # a field
+    hack += '1' if 'M' in comp else '0'
+
+    # comp field
+    if comp not in comptab:
+        return None
+    hack += comptab[comp]
+
+    # dest field
+    if not dest in desttab:
+        return None
+    hack += desttab[dest]
+
+    # jump field
+    if jump not in jumptab:
+        return None
+    hack += jumptab[jump]
+
+    hack += '\n'
+
+    return hack
+
+
+def asm2hack(asm):
+    if asm.startswith('@'):
+        return a_cmd(asm)
+    else:
+        return c_cmd(asm)
+
---
+++ Schooling/n2t/06/symbol.py
@@ -0,0 +1,63 @@
+import json
+import re
+
+re_symbol = re.compile('[$:.\w]+')
+digit = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') 
+addr = 15
+
+with open('tables.json') as f:
+    symtab = json.load(f)['symbol']
+
+
+def not_valid(sym):
+    return not re_symbol.fullmatch(sym) or sym.startswith(digit)
+
+
+def next_addr():
+    global addr
+    addr += 1
+    if addr < 16:
+        raise ValueError('attempt to bind reserved memory to symbol\n'
+                         'attempted value: {}'.format(addr))
+    return addr
+
+
+def install_label(name, defn):
+
+    if not name.endswith(')'):
+        return None
+
+    label = name[1:-1]  # strip outer parens
+
+    if not_valid(label):
+        return None
+
+    if label in symtab:
+        return None
+
+    symtab[label] = defn
+
+    return symtab[label]
+
+
+def install_symbol(name):
+
+    if not_valid(name):
+        return None
+
+    symtab[name] = next_addr()
+
+    return symtab[name]
+
+
+def install(name, defn=None):
+
+    if defn is None:
+        return install_symbol(name)
+    else:
+        return install_label(name, defn)
+
+
+def lookup(name):
+    return symtab.get(name)
+
