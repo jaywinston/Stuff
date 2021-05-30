f264ba9ab5788e197e2f05bf36b39b6348d50933
Jay Winston <>
2021-05-30 12:21:55 +0300

    ranges.c might be a solution
     On branch master
     Changes to be committed:
    	modified:   Schooling/ansic/src/2/ranges.c
    	modified:   Schooling/n2t/02/ALU.hdl
    	modified:   Schooling/n2t/09/Turtle/Eval.jack
    	modified:   Schooling/n2t/09/Turtle/Exec.jack
    	modified:   Schooling/n2t/09/Turtle/LibSym.jack

--- Schooling/ansic/src/2/ranges.c
+++ Schooling/ansic/src/2/ranges.c
@@ -1,47 +1,44 @@
 #include <stdio.h>
 #include <limits.h>
-#include <float.h>
 
+/* unfinished */
 
-int minn(int size)
+/* step through all states from from "0" to "~0" capturing the maximum and
+ * minimum values.
+ * use bitwise operations because data representation is not known
+ */
+int main(int c, char **v)
 {
-    return 1 << size * 8 - 1;
-}
+  unsigned char onebit;
 
+  {
+    unsigned char x = ~(0 ^ 0);
+    int bitcount = 0;  /* minus one :\ */
+    while (x <<= 1)
+      ++bitcount;
+    onebit = ((unsigned char) ~(0 ^ 0)) >> bitcount;
+  }
 
-int maxn(int size)
-{
-    return ~minn(size);
-}
-
-
-int main ()
-{
-    printf("From \"limits.h\"\n============\n");
-    printf("char signed %20d, %d\n", SCHAR_MIN, SCHAR_MAX);
-    printf("char unsigned max %19d\n", UCHAR_MAX);
-    printf("short signed %17d, %d\n", SHRT_MIN, SHRT_MAX);
-    printf("short unsigned max %18u\n", USHRT_MAX);
-    printf("int signed %14d, %d\n", INT_MIN, INT_MAX);
-    printf("int unsigned max %20u\n", UINT_MAX);
-    printf("long signed %13ld, %ld\n", LONG_MIN, LONG_MAX);
-    printf("long unsigned max %19u\n", ULONG_MAX);
-    printf("float %13lf, %lf\n", FLT_MIN, FLT_MAX);
-    printf("double %13lf, %lf\n", DBL_MIN, DBL_MAX);
-    printf("long double %19lf, %lf\n", LDBL_MIN, LDBL_MAX);
-    putchar('\n');
-
-    printf("Calculated\n==========\n");
-
-    /* I don't know how this can be calculated without
-       knowledge of the machine's architecture. */
-
-    printf("char signed %d, %d\n", maxn(sizeof (char)), minn(sizeof (char)));
-    printf("char unsigned max %19d\n", maxn(sizeof (unsigned char)));
-    printf("short signed %10d .. %10d\n", maxn(sizeof (short)), minn(sizeof (short)));
-    printf("short unsigned max %18u\n", maxn(sizeof (unsigned short)));
-    printf("int signed %12d .. %d\n", maxn(sizeof (int)), minn(sizeof (int)));
-    printf("int unsigned max %20u\n", maxn(sizeof (unsigned int)));
-    printf("long signed %ld .. %ld\n", maxn(sizeof (long)), minn(sizeof (long)));
-    printf("long unsigned max %19u\n", maxn(sizeof (unsigned long)));
+  unsigned int canvas = 0 ^ 0;
+  int max = 0, min = 0;
+  unsigned int umax = 0, umin = 0;
+  do {
+    if (((int) canvas) > max)
+      max = (int) canvas;
+    if (((int) canvas) < min)
+      min = (int) canvas;
+    if (canvas > umax)
+      umax = canvas;
+    if (canvas < umin)
+      umin = canvas;
+    int bit;
+    for (bit = onebit; canvas & bit; bit <<= 1)
+      canvas &= ~bit;
+    canvas |= bit;
+  } while (canvas);
+  printf("Header File Maximum:         %12d\n", INT_MAX);
+  printf("Calculated Maximum:          %12d\n", max);
+  printf("Header File Unsigned Maximum:%12d\n", UINT_MAX);
+  printf("Calculated Unsigned Maximum: %12u\n", umax);
+  return 0;
 }
